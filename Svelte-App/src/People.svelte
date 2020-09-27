150d3130d953861779ab5d199141d1c68f60cda1
Jakob <jade@likeavulin.dk>
2020-09-27 21:44:18 +0200

    svelte default image on person

--- Svelte-App/src/People.svelte
+++ Svelte-App/src/People.svelte
@@ -6,7 +6,7 @@
 
   let people;
   let title = 'Personer';
-
+  let url_default = "https://storagejakob.blob.core.windows.net/blob-container-test/default-horse.jpg";
   onMount(async () => {
     const response = await fetch(
       `https://sveltehorsefunctionapp.azurewebsites.net/api/person`,
@@ -23,13 +23,18 @@
   {#if people}
     <h2>Personer</h2>
     <ul>
-      {#each people as { firstName, lastName, birthday, uri }, index}
+      {#each people as { firstName, lastName, birthday, url }, index}
         <li in:fly={{ x: 200, delay: index * 100 }} out:fly={{ x: -200 }}>
           <Hoverable let:hovering={active}>
+            {index + 1}: {firstName} {lastName} - {birthday}
             <div class:active>
               {#if active}
-                <img href={uri} alt="Min hest {uri}" />
-              {:else}{index + 1}: {firstName} {lastName} - {birthday}{/if}
+                {#if url}
+                  <img transition:blur src={url} alt="Min hest" width="200px" />
+                {:else}
+                  <img transition:blur src={url_default} alt="Ingen hest" width="150px"  />
+                {/if}
+              {/if}
             </div>
           </Hoverable>
         </li>
